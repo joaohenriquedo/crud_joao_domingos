91620299bf987abd33df75dbaadaf7f24a2d7965
joaohenriquedo <joaohenriquedomingos2008@gmail.com>
2025-02-25 21:03:40 -0300

    banco do crud

---
+++ joaodomingos__db.sql
@@ -0,0 +1,11 @@
+date base joaodomingos__db
+
+
+create table usuario(
+     idusuario int not null ,
+     nome text,
+     telefine text,
+     usuario text,
+     senha text,
+     primary key (idusuario)
+);
